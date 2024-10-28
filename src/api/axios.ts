60005e6ee59d9916f8ca336c6ae2ee4b431f8ac1
Salo Lobato <salo.lobato@gmail.com>
2024-10-28 19:58:08 -0300

    Adicionar URL de ambiente

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,29 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
 export const api = axios.create({
-  baseURL: "https://localhost:44374/api/",
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("token")
+  console.log(baseURL);
+  if (!!token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config;
+});
+
+export type ArrayResponse<T> = {
+  items: {
+    $values: T[],
+  },
+  currentPage: number,
+  pageSize: number,
+  totalCount: 1
+}
