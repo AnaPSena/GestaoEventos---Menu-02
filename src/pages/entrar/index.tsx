da11345614d68a1fde6b9f75bdcb4e0cf18e73f6
ginadev-dutra <gina_20_01@hotmail.com>
2024-11-02 14:56:34 -0300

    Configurando popup de senha incorreta

--- src/pages/entrar/index.tsx
+++ src/pages/entrar/index.tsx
@@ -3,6 +3,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { login, LoginSchema } from '@/api/usuarios';
 import Title from '@/components/title';
 import { Input } from '@/components/input';
+import { useState } from 'react';
 
 export default function Login() {
   const {
@@ -10,13 +11,14 @@ export default function Login() {
     handleSubmit,
     formState: { errors },
   } = useForm<LoginSchema>();
-
+  
+  const [showError, setShowError] = useState(false); // Controla a exibição do pop-up de erro
   const router = useRouter();
 
   const onSubmit: SubmitHandler<LoginSchema> = async (data) => {
     try {
-
-      const response = await login(data)
+      const response = await login(data);
+      
       // Salvar o token no LocalStorage
       localStorage.setItem('token', response.data.token);
 
@@ -24,13 +26,30 @@ export default function Login() {
       router.push('/eventos');
     } catch (error) {
       console.error('Erro no login:', error);
+      setShowError(true); // Exibe o pop-up em caso de erro
     }
   };
 
   return (
     <>
       <Title>Fazer login</Title>
-      <div className='w-80 mx-auto'>
+
+      {/* Condicional para exibir o pop-up de erro */}
+      {showError && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-4 rounded shadow-lg text-center">
+            <p className="text-red-500 font-semibold">Senha incorreta</p>
+            <button
+              className="mt-4 bg-astronaut-800 text-white rounded-md hover:bg-pizazz-500 p-2"
+              onClick={() => setShowError(false)} // Fecha o pop-up
+            >
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div className="w-80 mx-auto">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-10">
             <Input label="E-mail" path="Email" register={register} required type="email" />
